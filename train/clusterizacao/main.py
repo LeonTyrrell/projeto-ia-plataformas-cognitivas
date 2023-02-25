a85c741b99cae22baf0dbce46c2c11897c8c92ed
Leon_tyrrell <leovictorlopes@hotmail.com>
2023-02-25 13:22:00 -0300

    melhoria na clusterização com genero

--- train/clusterizacao/main.py
+++ train/clusterizacao/main.py
@@ -1,6 +1,16 @@
+''' ?Exemplo de post
+{
+	"income": 4980,
+	"age": "55-64",
+	"year": 2019,
+	"gender": "Female"
+}
+'''
+
+
+
 # fastapi==0.92.0
 # uvicorn==0.20.0 pip install "uvicorn[standard]"
-from sklearn.preprocessing import LabelEncoder
 
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
@@ -16,9 +26,10 @@ app = FastAPI()
 
 class Cont(BaseModel):
     income:int
-    age:str
+    age:str # ['25-34', '35-44', '45-54', '55-64', '65-74', '<25', '>74']
     year:int
-    Credit_Score:int
+    # Credit_Score:int
+    gender:str # ['Female', 'Joint', 'Male', 'Sex Not Available']
 
 origins = ['*']
 
@@ -31,26 +42,36 @@ app.add_middleware(
 )
 
 
-@app.post('/contador')
+@app.post('/')
 def teste(cont:Cont):
     # Carrega o kmeans,labelencoder,scaler de normalização
-    kmeans, le, scaler = load('cluster.joblib'), load('labelencoder.joblib'),load('scaler.joblib')
+    kmeans, le_age,le_gender, scaler = load('cluster.joblib'), load('labelencoder_age.joblib'),load('labelencoder_gender.joblib'),load('scaler.joblib')
     df = pd.DataFrame({
         "income": [cont.income],
-        "age": le.transform([cont.age]),
+        "age": le_age.transform([cont.age]),
         "year": [cont.year],
-        "Credit_Score": [cont.Credit_Score]
+        # "Credit_Score": [cont.Credit_Score],
+        "Gender": le_gender.transform([cont.gender])
     })
     # Resultado da clusterização
     result = kmeans.predict(scaler.transform(df))
     # Resultado da clusterização
 
     # Média por cluster 
-    media = (0.238315,0.279129,0.238814,0.274846,0.231080,0.362069)
+    media = (0.264404,0.208608,0.246458,0.372881,0.304962,0.224107)
     # Média por cluster 
+    classi = (
+                "Masculino e LGBT com risco de não pagamento",     # 0
+                "Feminino e LGBT de alta renda",                   # 1
+                "Feminino e LGBT de media renda de 3° idade",      # 2
+                "Homens 2° idade de alta renda",                   # 3
+                "Sem definição de sexo oui homens de baixa renda", # 4
+                "Risco de não pagamento"                           # 5
+            )
     
     response = {
         'propensao_media': media[result[0]],
-        'grupo': int(result[0])
+        'grupo': int(result[0]),
+        "classificacao": classi[result[0]] 
     }
     return JSONResponse(content=response)
